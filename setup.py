b69f6320764bba8a52346d691ae5cdb09183fda3
Dalton Barreto <daltonmatos@gmail.com>
2010-11-06 17:16:36 -0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+from distutils.core import setup
+
+
+setup(
+  name="plugnplay",
+  version="-next",
+  url="http://github.com/daltonmatos/plugnplay",
+  license="GPLv2",
+  description="A Generic plug-in system for python",
+  author="Dalton Barreto",
+  packages=['plugnplay'])
