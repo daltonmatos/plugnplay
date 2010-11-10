2c91e5a729ecd89ae0c74eea03c0e6cfae90638f
Dalton Barreto <daltonmatos@gmail.com>
2010-11-10 21:12:40 -0200

    Changed from distutils to setuptools (to create egg packages). Bumped version.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 # encoding: utf-8
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
   name="plugnplay",
-  version="-next",
+  version="0.1",
   url="http://github.com/daltonmatos/plugnplay",
   license="GPLv2",
   description="A Generic plug-in system for python",
