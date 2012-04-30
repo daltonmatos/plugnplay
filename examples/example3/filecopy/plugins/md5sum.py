a52037ac8933b461afa5fb997d7cd731a18f41bd
Jeff Hinrichs <dundeemt@gmail.com>
2012-04-29 20:03:48 -0500

    purged commented code, made code look the same across modules, added pycharm settings folder to ignored

--- examples/example3/filecopy/plugins/md5sum.py
+++ examples/example3/filecopy/plugins/md5sum.py
@@ -1,35 +1,34 @@
 import hashlib
 import sys
 
-# We have to do this so we can use HashChecker without
-# Installing this example with python setup install
-#dirname = os.path.dirname(os.path.abspath(__file__))
-#up_dir =  os.path.dirname(dirname)
-#sys.path.append(up_dir)
 
 from interfaces import HashChecker
 import plugnplay
 
 
 class MD5(plugnplay.Plugin):
-  implements = [HashChecker]
+    implements = [HashChecker]
 
-  def _readFile(self, fName):
-      try:
-        return open(fName, encoding='utf-8').read().encode('utf-8')
-      except TypeError:
-          return open(fName).read().encode('utf-8')
+    def _readFile(self, fName):
+        """
+        Some work around code, to handle python versions that don't all specifying encoding in the open statement
+        """
+        try:
+            return open(fName, encoding='utf-8').read().encode('utf-8')
+        except TypeError:   #py2.6 doesn't support encoding= param
+            return open(fName).read().encode('utf-8')
 
-  def check(self, file1, file2):
-    """
-    implement the check method enforced in the Interface
-    """
-    md5_1 = hashlib.md5(self._readFile(file1)).hexdigest()
-    md5_2 = hashlib.md5(self._readFile(file2)).hexdigest()
-    if md5_1 == md5_2:
-      sys.stdout.write("MD5sum OK: %s\n" % md5_1)
-      return True
-    else:
-      return False
+
+    def check(self, file1, file2):
+        """
+        implement the check method enforced in the Interface
+        """
+        md5_1 = hashlib.md5(self._readFile(file1)).hexdigest()
+        md5_2 = hashlib.md5(self._readFile(file2)).hexdigest()
+        if md5_1 == md5_2:
+            sys.stdout.write("MD5sum OK: %s\n" % md5_1)
+            return True
+        else:
+            return False
 
 
