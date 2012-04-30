cc6285fc002fc128bd233c59810f2ce7e79aa77f
Jeff Hinrichs <dundeemt@gmail.com>
2012-04-29 23:29:32 -0500

    more cleaning, clean indenting, more purging of commented code

--- examples/example3/filecopy/interfaces.py
+++ examples/example3/filecopy/interfaces.py
@@ -2,11 +2,11 @@ from plugnplay import Interface
 
 class HashChecker(Interface):
 
-  '''
-    Called to check the hash of the two files
-    Receive the two files: original and copy
-    return True if the copy was OK or False if some error ocurred
-  '''
-  def check(self, original_file, dup_file):
-    pass
+    '''
+      Called to check the hash of the two files
+      Receive the two files: original and copy
+      return True if the copy was OK or False if some error ocurred
+    '''
+    def check(self, original_file, dup_file):
+        pass
 
