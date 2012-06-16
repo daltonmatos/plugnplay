a0b59ecb4257cea9fc0c152938eaec69ac3d76af
Dalton Barreto <daltonmatos@gmail.com>
2012-06-16 09:44:43 -0300

    PEP8 adjustments

--- tests/manager.py
+++ tests/manager.py
@@ -1,41 +1,42 @@
 # encoding: utf-8
-
 import unittest
 from plugnplay import Manager, Interface
 
 
 class SomeInterface(Interface):
-  pass
+    pass
+
 
 class PlugA:
-  pass
+    pass
+
 
 class PlugB:
-  pass
+    pass
 
 
 class ManagerTest(unittest.TestCase):
 
-  def setUp(self):
-    self.man = Manager()
+    def setUp(self):
+        self.man = Manager()
 
-  '''
-    Tests that the managar can register one implementor
-    of one interface
-  '''
-  def test_add_first_implementor_of_an_interface(self):
-    self.man.add_implementor(SomeInterface, PlugA())
-    self.assertEquals(1, len(self.man.implementors(SomeInterface)))
-    self.assertTrue(isinstance(self.man.implementors(SomeInterface)[0], PlugA))
+    '''
+        Tests that the managar can register one implementor
+        of one interface
+    '''
+    def test_add_first_implementor_of_an_interface(self):
+        self.man.add_implementor(SomeInterface, PlugA())
+        self.assertEquals(1, len(self.man.implementors(SomeInterface)))
+        self.assertTrue(isinstance(self.man.implementors(SomeInterface)[0], PlugA))
 
-  def test_add_second_implementor_of_an_interface(self):
-    self.man.add_implementor(SomeInterface, PlugA())
-    self.man.add_implementor(SomeInterface, PlugB())
-    self.assertEquals(2, len(self.man.implementors(SomeInterface)))
+    def test_add_second_implementor_of_an_interface(self):
+        self.man.add_implementor(SomeInterface, PlugA())
+        self.man.add_implementor(SomeInterface, PlugB())
+        self.assertEquals(2, len(self.man.implementors(SomeInterface)))
 
-  '''
-    The list of implementors of a Interface should be empty if it has nos
-    implementors yet.
-  '''
-  def test_return_empty_list_of_implementors(self):
-    self.assertEquals(0, len(self.man.implementors(SomeInterface)))
+    '''
+        The list of implementors of a Interface should be empty if it has nos
+        implementors yet.
+    '''
+    def test_return_empty_list_of_implementors(self):
+        self.assertEquals(0, len(self.man.implementors(SomeInterface)))
